c35156c01b8aada26ece05a31c7ff2fb3ab4c338
jfparrado <jnsparrado@hotmail.com>
2023-04-25 13:38:22 -0500

    se crea el auth context y esta funcional

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import store from "./store/index";
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <React.StrictMode>
+    <BrowserRouter>
       <App />
-    </React.StrictMode>
+    </BrowserRouter>
   </Provider>,
 )
